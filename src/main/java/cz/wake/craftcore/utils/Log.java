1b7ed4e427bcf9946d05fd07fa3223724d295427
MrWakeCZ <joseff.kral@gmail.com>
2017-12-01 16:26:39 +0100

    Úprava popisků logů

--- src/main/java/cz/wake/craftcore/utils/Log.java
+++ src/main/java/cz/wake/craftcore/utils/Log.java
@@ -5,12 +5,32 @@ import org.bukkit.ChatColor;
 
 public class Log {
 
-    public static void withPrefix(String s) {
-        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "[CraftCore] " + ChatColor.WHITE + s);
+    /**
+     * Metoda zasilajici do konzole zpravu s preddefinovanym prefixem CraftCore
+     *
+     * @param text Text zprávy
+     */
+    public static void withPrefix(String text) {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "[CraftCore] " + ChatColor.WHITE + text);
     }
 
-    public static void normalMessage(String s) {
-        Bukkit.getConsoleSender().sendMessage(s);
+    /**
+     * Metoda zasilajici do konzole zpravu s vlastnim prefixem.
+     *
+     * @param customPrefix Prefix
+     * @param text Text zprávy
+     */
+    public static void withCustomPrefix (String customPrefix, String text){
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "[" + customPrefix + "]" + ChatColor.WHITE + text);
+    }
+
+    /**
+     * Zprava zasilajici do konzole text bez prefixu a formátování
+     *
+     * @param text Text zprávy
+     */
+    public static void normalMessage(String text) {
+        Bukkit.getConsoleSender().sendMessage(text);
     }
 
 }

4cc17e8e74f98e6fe88bcb1559427dc42a2d4c32
MrWakeCZ <joseff.kral@gmail.com>
2018-01-17 20:53:39 +0100

    Odebráno HikariCP

--- src/main/java/cz/wake/craftcore/Main.java
+++ src/main/java/cz/wake/craftcore/Main.java
@@ -7,7 +7,6 @@ import cz.wake.craftcore.listener.basic.PlayerJoinListener;
 import cz.wake.craftcore.listener.basic.PlayerLeaveListener;
 import cz.wake.craftcore.listener.extended.*;
 import cz.wake.craftcore.listener.worldguard.WGRegionEventsListener;
-import cz.wake.craftcore.sql.SQLManager;
 import cz.wake.craftcore.tasks.TpsPollerTask;
 import cz.wake.craftcore.utils.Log;
 import org.bukkit.Bukkit;
@@ -16,16 +15,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.eclipse.jetty.server.Server;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends JavaPlugin {
 
-    private Server server;
     private String idServer;
-    private SQLManager sql;
     private WGRegionEventsListener wgListener;
     private WorldGuardPlugin wgPlugin;
     private static List<Player> effectPlayers = new ArrayList<>();
@@ -47,9 +43,6 @@ public class Main extends JavaPlugin {
         // Register eventu a prikazu
         loadListeners();
         loadCommands();
-
-        // HikariCP
-        initDatabase();
 
         // WorldGuard Addons
         this.wgPlugin = this.getWGPlugin();
@@ -75,10 +68,6 @@ public class Main extends JavaPlugin {
     }
 
     public void onDisable() {
-
-        // Deaktivace HikariCP
-        sql.onDisable();
-
         instance = null;
     }
 
@@ -114,14 +103,6 @@ public class Main extends JavaPlugin {
         protocolManager.addPacketListener(new PlayerChangeGameStateListener(this));
         protocolManager.addPacketListener(new PlayerReceiveStatisticsEventListener(this));
         protocolManager.addPacketListener(new PlayerReceiveMessageEventListener(this));
-    }
-
-    private void initDatabase() {
-        sql = new SQLManager(this);
-    }
-
-    public SQLManager getSQL() {
-        return this.sql;
     }
 
     private WorldGuardPlugin getWGPlugin() {

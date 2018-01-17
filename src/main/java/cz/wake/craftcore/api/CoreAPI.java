4589aff29d9bb8e35651b8847fddd356b2023a89
MrWakeCZ <joseff.kral@gmail.com>
2018-01-17 21:19:58 +0100

    Zbytek oprav...

--- src/main/java/cz/wake/craftcore/api/CoreAPI.java
+++ src/main/java/cz/wake/craftcore/api/CoreAPI.java
@@ -1,8 +1,6 @@
 package cz.wake.craftcore.api;
 
 import cz.wake.craftcore.Main;
-import cz.wake.craftcore.annotations.NeedTesting;
-import cz.wake.craftcore.sql.SQLManager;
 
 public class CoreAPI {
 
@@ -13,24 +11,5 @@ public class CoreAPI {
      */
     public static Main getCoreInstance() {
         return Main.getInstance();
-    }
-
-    /**
-     * Metoda k ziskani instance SQLManageru
-     *
-     * @return SQLManager
-     */
-    @NeedTesting
-    public SQLManager getSQL() {
-        return Main.getInstance().getSQL();
-    } //TODO: Fix NPE
-
-    /**
-     * Metoda k ziskani IDcka serveru (podle configu)
-     *
-     * @return ID serveru
-     */
-    public static String getIdServer() {
-        return Main.getInstance().getIdServer();
     }
 }

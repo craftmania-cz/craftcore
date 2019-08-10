72a1fa13b57c24f63666661fb09822da79a69c87
MrWakeCZ <joseff.kral@gmail.com>
2019-08-10 17:04:11 +0200

    Compilace odebraných libs v 1.14.4

--- spigot/src/main/java/cz/craftmania/craftcore/spigot/Main.java
+++ spigot/src/main/java/cz/craftmania/craftcore/spigot/Main.java
@@ -97,10 +97,11 @@ public final class Main extends JavaPlugin {
         ServerData.getInstance().setPluginVersion(this);
         new SkinAPI();
         new BungeeAPI();
-        for (String proxy : getConfig().getStringList("proxies")) {
+        //TODO: Nefunkční na 1.14.4 (java.lang.NoClassDefFoundError: org/apache/commons/lang3/builder/HashCodeBuilder)
+        /*for (String proxy : getConfig().getStringList("proxies")) {
             String[] x = proxy.split(":");
             ProxyUtils.put(x[0], Integer.parseInt(x[1]));
-        }
+        }*/
 
         // Inventory Builder
         invManager = new InventoryManager(this);

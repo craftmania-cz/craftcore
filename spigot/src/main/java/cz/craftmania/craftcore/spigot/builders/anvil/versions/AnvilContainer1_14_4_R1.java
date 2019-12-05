f69bfe0972494fab2f4efcf2cc8edbf4a49b8f42
MrWakeCZ <joseff.kral@gmail.com>
2019-12-05 01:40:21 +0100

    Přidáno nové AnvilGUI pro 1.14.4+

---
+++ spigot/src/main/java/cz/craftmania/craftcore/spigot/builders/anvil/versions/AnvilContainer1_14_4_R1.java
@@ -0,0 +1,30 @@
+package cz.craftmania.craftcore.spigot.builders.anvil.versions;
+
+import net.minecraft.server.v1_14_R1.BlockPosition;
+import net.minecraft.server.v1_14_R1.ChatMessage;
+import net.minecraft.server.v1_14_R1.ContainerAccess;
+import net.minecraft.server.v1_14_R1.ContainerAnvil;
+import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class AnvilContainer1_14_4_R1 extends ContainerAnvil {
+
+    public AnvilContainer1_14_4_R1(Player player, int containerId) {
+        super(containerId, ((CraftPlayer) player).getHandle().inventory,
+                ContainerAccess.at(((CraftWorld) player.getWorld()).getHandle(), new BlockPosition(0, 0, 0)));
+        this.checkReachable = false;
+        setTitle(new ChatMessage("Repair & Name"));
+    }
+
+    @Override
+    public void e() {
+        super.e();
+    }
+
+    public int getContainerId() {
+        return windowId;
+    }
+
+}
+

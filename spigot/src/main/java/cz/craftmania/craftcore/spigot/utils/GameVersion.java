95c4a64c9bb8c3c32aa7d430ca9a72a1d5aac382
MrWakeCZ <joseff.kral@gmail.com>
2020-11-07 12:05:21 +0100

    Podpora pro 1.16.4

--- spigot/src/main/java/cz/craftmania/craftcore/spigot/utils/GameVersion.java
+++ spigot/src/main/java/cz/craftmania/craftcore/spigot/utils/GameVersion.java
@@ -10,17 +10,18 @@ public enum GameVersion {
     v1_8_R3(2),
     v1_9_R1(3),
     v1_9_R2(4),
-
-    // Supported by CraftCore
     v1_10_R1(5),
     v1_11_R1(6),
     v1_12_R1(7),
     v1_13_R1(8),
     v1_13_R2(9),
     v1_14_R1(10),
+
+    // Supported by CraftCore
     v1_15_R1(11),
     v1_16_R1(12),
-    v1_16_R2(13);
+    v1_16_R2(13),
+    v1_16_R3(14);
 
     private static final GameVersion version = GameVersion.valueOf(Bukkit.getServer().getClass().getPackage()
             .getName().replace(".", ",").split(",")[3]);

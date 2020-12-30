9bec9cde08cd2b99f227a6f06e8283dc04efd457
MrWakeCZ <joseff.kral@gmail.com>
2020-12-31 00:19:36 +0100

    Odebráno kompletně bungeecord core

--- bungee/src/main/java/cz/craftmania/craftcore/bungee/annotations/PacketHandler.java
+++
@@ -1,9 +0,0 @@
-package cz.craftmania.craftcore.bungee.annotations;
-
-import java.lang.annotation.*;
-
-@Inherited
-@Target(value = ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface PacketHandler {
-}

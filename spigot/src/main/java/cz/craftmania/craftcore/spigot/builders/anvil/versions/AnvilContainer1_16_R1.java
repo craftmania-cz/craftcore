8a3e709afe541b8fec9a21da5d245bd9e5b502ba
MrWakeCZ <joseff.kral@gmail.com>
2021-06-19 21:53:17 +0200

    Deaktivace AnvilAPI (NMS nejdou)

--- spigot/src/main/java/cz/craftmania/craftcore/spigot/builders/anvil/versions/AnvilContainer1_16_R1.java
+++ spigot/src/main/java/cz/craftmania/craftcore/spigot/builders/anvil/versions/AnvilContainer1_16_R1.java
@@ -1,17 +1,17 @@
 package cz.craftmania.craftcore.spigot.builders.anvil.versions;
 
-import net.minecraft.server.v1_16_R3.BlockPosition;
-import net.minecraft.server.v1_16_R3.ChatMessage;
-import net.minecraft.server.v1_16_R3.ContainerAccess;
-import net.minecraft.server.v1_16_R3.ContainerAnvil;
-import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
-import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+import net.minecraft.core.BlockPosition;
+import net.minecraft.network.chat.ChatMessage;
+import net.minecraft.world.inventory.ContainerAccess;
+import net.minecraft.world.inventory.ContainerAnvil;
+import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class AnvilContainer1_16_R1 extends ContainerAnvil {
 
     public AnvilContainer1_16_R1(Player player, int containerId) {
-        super(containerId, ((CraftPlayer) player).getHandle().inventory,
+        super(containerId, ((CraftPlayer) player).getHandle().getInventory(),
                 ContainerAccess.at(((CraftWorld) player.getWorld()).getHandle(), new BlockPosition(0, 0, 0)));
         this.checkReachable = false;
         setTitle(new ChatMessage("Repair & Name"));
@@ -22,9 +22,10 @@ public class AnvilContainer1_16_R1 extends ContainerAnvil {
         super.e();
     }
 
-    public int getContainerId() {
-        return windowId;
-    }
+    //TODO: Fix
+    //public int getContainerId() {
+    //    return windowId;
+    //}
 
 }
 
